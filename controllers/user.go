ee2e6cb0feb245e077b10948f7d8687269f865a7
unknown <majidulaziz11@gmail.com>
2022-10-26 16:11:12 +0700

    registrasi

--- controllers/user.go
+++ controllers/user.go
@@ -3,7 +3,7 @@ package controllers
 import (
 	"rapid/m-holding/database"
 	"rapid/m-holding/models"
-
+	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
 
@@ -19,3 +19,34 @@ func InitUserController() *UserController {
 
 	return &UserController{Db: db}
 }
+
+//GET FORM REGISTRASI
+func (controller *UserController) Register(c *fiber.Ctx) error {
+	return c.Render("registrasi", fiber.Map{
+		"Title": "Register User",
+	})
+}
+
+//POST TO REGISTRASI
+func (controller *UserController) NewRegister(c *fiber.Ctx) error {
+	//myform := new(models.Product)
+	var registrasi models.User
+
+	if err := c.BodyParser(&registrasi); err != nil {
+		return c.Redirect("/registrasi")
+	}
+	// save registrasi
+	err := models.Registrasi(controller.Db, &registrasi)
+	if err != nil {
+		return c.Redirect("/registrasi")
+	}
+	// if succeed
+	return c.Redirect("/login")
+}
+
+func (controller *UserController) Login(c *fiber.Ctx) error {
+	return c.Render("login", fiber.Map{
+		"Title": "Login",
+	})
+}
+
--- models/user.go
+++ models/user.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"gorm.io/gorm"
+	"golang.org/x/crypto/bcrypt"
+	"fmt"
 )
 
 type User struct {
@@ -12,10 +14,16 @@ type User struct {
 	Email    string `form:"email" json: "email" validate:"required"`
 	Role     string `form:"role" json: "role" validate:"required"`
 	Password string `form:"password" json: "password" validate:"required"`
+	Disabel  bool `gorm:"default:0"`
 	TenantID uint
 }
 
-func CreateUser(db *gorm.DB, newUser *User) (err error) {
+func Registrasi(db *gorm.DB, newUser *User) (err error) {
+	plainPassword := newUser.Password
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(plainPassword),10)
+	sHash := string(bytes)
+	fmt.Println("Hash password: ", sHash)
+	newUser.Password = sHash
 	err = db.Create(newUser).Error
 	if err != nil {
 		return err
@@ -23,10 +31,10 @@ func CreateUser(db *gorm.DB, newUser *User) (err error) {
 	return nil
 }
 
-func FindUserByUsername(db *gorm.DB, user *User, username string) (err error) {
-	err = db.Where("username=?", username).First(user).Error
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// func FindUserByUsername(db *gorm.DB, user *User, username string) (err error) {
+// 	err = db.Where("username=?", username).First(user).Error
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
